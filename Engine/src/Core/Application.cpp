ea08324f32b84b2014f582c3310bd45495635ea5
michal-swiatek <swiatek.michal006@gmail.com>
2021-04-18 18:25:44 +0200

    Reincluded spdlog submodule (repo restored!)

--- Engine/src/Core/Application.cpp
+++ Engine/src/Core/Application.cpp
@@ -16,14 +16,12 @@ void Application::Run()
 {
     std::cout << "Hello world" << std::endl;
 
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         exit(-1);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
