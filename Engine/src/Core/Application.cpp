81e53e7ba8c9b5857ae6c8926c73a25776775fbe
michal-swiatek <swiatek.michal006@gmail.com>
2021-04-18 17:58:01 +0200

    Added glfw with basic example

--- Engine/src/Core/Application.cpp
+++ Engine/src/Core/Application.cpp
@@ -7,9 +7,44 @@
 #include "pch.h"
 #include "Core/Application.h"
 
+#include <GLFW/glfw3.h>
+#include <cstdlib>
+
 using namespace engine;
 
 void Application::Run()
 {
     std::cout << "Hello world" << std::endl;
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        exit(-1);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(-1);
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+//        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
